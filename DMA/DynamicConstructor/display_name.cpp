b91fdaf5d7d84e43489b3699cdfb263876de45fb
n3vilreal <nebil.241423@ncit.edu.np>
2025-06-05 09:31:50 +0545

    program add

---
+++ DMA/DynamicConstructor/display_name.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class Demo
+{
+    private:
+        char *p;
+    public:
+        Demo()
+        {
+            p = new char [1];
+            strcpy(p,"");
+        }
+        Demo(const char *s)
+        {
+            p = new char(strlen(s)+1);
+            strcpy(p,s);
+        }
+        ~Demo()
+        {
+            delete[] p;
+        }
+        void display()
+        {
+            cout<<"Name is: "<<p<<endl;
+        }
+};
+int main()
+{
+    Demo d1("Ram ");
+    Demo d2("Sharma Adhikari");
+    d1.display();
+    d2.display();
+    return 0;
+}
